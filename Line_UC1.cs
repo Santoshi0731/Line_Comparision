56975d61ab87f32919527204dc8d0f1a61abc05f
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-01-05 12:20:00 +0530

    Add files via upload

---
+++ Line_UC1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class Line_UC1
+    {
+        public static void line1()
+        {
+            int x1, x2, y1, y2, x, y;
+
+            double z, a;
+
+            Console.WriteLine("enter point (x1,y1)");
+            x1= Convert.ToInt32(Console.ReadLine());
+            y1= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter point (x2,y2)");
+
+            x2= Convert.ToInt32(Console.ReadLine());
+            y2= Convert.ToInt32(Console.ReadLine());
+
+
+            x =(x2-x1);
+            y =(y2-y1);
+            z=Math.Pow(x,2)+Math.Pow(y,2);
+            a=Math.Sqrt(z);
+            Console.WriteLine("length of line = " +a);
+
+
+        }
+    }
+}
